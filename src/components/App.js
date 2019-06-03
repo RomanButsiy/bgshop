5f23c73e7f676070d09c1c03f08eb6a7d00bbf4a
RomanButsiy <butsiyroman@gmail.com>
2019-06-04 00:57:46 +0300

    Render List of Components

---
+++ src/components/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import GameList from './GameList';
+
+const games = [
+    {
+        _id: 1,
+        name: "Quadropolis",
+        thumbnail: "https://cf.geekdo-images.com/imagepagezoom/img/_8vPsfrLL8yO7VzTQA1VkjTfsqM=/fit-in/1200x900/filters:no_upscale()/pic2840020.jpg",
+        price: "32.99",
+        players: "2-4",
+        duration: "60"
+    },
+    {
+        _id: 2,
+        name: "Five Tribes",
+        thumbnail: "https://cf.geekdo-images.com/imagepagezoom/img/duz9VImXFJLPAOeMfyt-pLzV0dM=/fit-in/1200x900/filters:no_upscale()/pic2055255.jpg",
+        price: "51.00",
+        players: "2-4",
+        duration: "80"
+    },
+    {
+        _id: 3,
+        name: "Roll for the Galaxy",
+        thumbnail: "https://cf.geekdo-images.com/imagepagezoom/img/M6HD1apPIhmhBuvGA8hxPjXGp90=/fit-in/1200x900/filters:no_upscale()/pic1473629.jpg",
+        price: "29.99",
+        players: "2-5",
+        duration: "45"
+    }
+];
+
+const App = () => (
+    <div className="ui container">
+        <GameList games={games} />
+    </div>
+);
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,34 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-import GameCard from './components/GameCard'
+import App from './components/App'
 import 'semantic-ui-css/semantic.min.css';
 
-const games = [
-    {
-        name: "Quadropolis",
-        thumbnail: "https://cf.geekdo-images.com/imagepagezoom/img/_8vPsfrLL8yO7VzTQA1VkjTfsqM=/fit-in/1200x900/filters:no_upscale()/pic2840020.jpg",
-        price: "32.99",
-        players: "2-4",
-        duration: "60"
-    },
-    {
-        name: "Five Tribes",
-        thumbnail: "https://cf.geekdo-images.com/imagepagezoom/img/duz9VImXFJLPAOeMfyt-pLzV0dM=/fit-in/1200x900/filters:no_upscale()/pic2055255.jpg",
-        price: "51.00",
-        players: "2-4",
-        duration: "80"
-    },
-    {
-        name: "Roll for the Galaxy",
-        thumbnail: "https://cf.geekdo-images.com/imagepagezoom/img/M6HD1apPIhmhBuvGA8hxPjXGp90=/fit-in/1200x900/filters:no_upscale()/pic1473629.jpg",
-        price: "29.99",
-        players: "2-5",
-        duration: "45"
-    }
-];
 
-
-ReactDOM.render(<GameCard game={games[1]}/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
