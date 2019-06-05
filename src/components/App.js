23ca47be1c958747993665f8ae1f3817aeaa8a20
RomanButsiy <butsiyroman@gmail.com>
2019-06-05 10:19:29 +0300

    Conditionals and Default Props

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,30 @@
 import React from 'react';
-import GameList from './GameList';
+import GameList from './GamesList';
 
 const games = [
     {
         _id: 1,
         name: "Quadropolis",
         thumbnail: "https://cf.geekdo-images.com/imagepagezoom/img/_8vPsfrLL8yO7VzTQA1VkjTfsqM=/fit-in/1200x900/filters:no_upscale()/pic2840020.jpg",
-        price: "32.99",
+        price: 3299,
         players: "2-4",
-        duration: "60"
+        duration: 60
     },
     {
         _id: 2,
         name: "Five Tribes",
         thumbnail: "https://cf.geekdo-images.com/imagepagezoom/img/duz9VImXFJLPAOeMfyt-pLzV0dM=/fit-in/1200x900/filters:no_upscale()/pic2055255.jpg",
-        price: "51.00",
+        price: 5100,
         players: "2-4",
-        duration: "80"
+        duration: 80
     },
     {
         _id: 3,
         name: "Roll for the Galaxy",
         thumbnail: "https://cf.geekdo-images.com/imagepagezoom/img/M6HD1apPIhmhBuvGA8hxPjXGp90=/fit-in/1200x900/filters:no_upscale()/pic1473629.jpg",
-        price: "29.99",
+        price: 2999,
         players: "2-5",
-        duration: "45"
+        duration: 45
     }
 ];
 
