fc6ed655ed62369daffd5a87aa0255d07be18fa4
RomanButsiy <butsiyroman@gmail.com>
2019-06-13 21:53:53 +0300

    Textarea and Universal Event Handler

--- src/components/GameForm.js
+++ src/components/GameForm.js
@@ -3,28 +3,43 @@ import PropTypes from "prop-types";
 
 class GameForm extends Component {
     state = {
-        name: ''
+        name: "",
+        description: ""
     };
 
     handleSubmit = e => {
         e.preventDefault();
         console.log(this.state)
     };
+    handleChange = e => this.setState({ [e.target.name]: e.target.value })
 
-    handleNameChange = e => this.setState({ name: e.target.value })
     render() {
         return (
             <form className="ui form" onSubmit={this.handleSubmit}>
                 <div className="field">
-                    <label htmlFor="">
+                    <label htmlFor="name">
                         Game Title
                     </label>
                     <input 
                     type="text" 
-                    id="name" 
+                    id="name"
+                    name="name" 
                     placeholder="Full game title"
                     value={this.state.name}
-                    onChange={this.handleNameChange}
+                    onChange={this.handleChange}
+                    />
+                </div>
+                <div className="field">
+                    <label htmlFor="description">
+                        Game Description
+                    </label>
+                    <textarea 
+                    type="text" 
+                    id="description"
+                    name="description" 
+                    placeholder="Game description"
+                    value={this.state.description}
+                    onChange={this.handleChange}
                     />
                 </div>
                 <button className="ui button" type="submit">
