6c5bef9fda366711375d598d1761ed984c6a32a7
RomanButsiy <butsiyroman@gmail.com>
2019-06-13 23:02:49 +0300

    Checkboxes

--- src/components/GameForm.js
+++ src/components/GameForm.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
 import PropTypes from "prop-types";
 
+const tags = [
+    { _id: 1, name: "dice" },
+    { _id: 2, name: "economic" },
+    { _id: 3, name: "family" }
+];
+
 class GameForm extends Component {
     state = {
         name: "",
-        description: ""
+        description: "",
+        price: 0,
+        duration: 0,
+        players: "",
+        featured: false,
+        tags: []
     };
 
     handleSubmit = e => {
         e.preventDefault();
         console.log(this.state)
     };
-    handleChange = e => this.setState({ [e.target.name]: e.target.value })
+    handleStringChange = e => this.setState({ [e.target.name]: e.target.value });
+    handleNumberChange = e => this.setState({ [e.target.name]: parseInt(e.target.value, 10) });
+    handleCheckboxChange = e => this.setState({ [e.target.name]: e.target.checked });
+    toggleTag = tag => 
+        this.state.tags.includes(tag._id)
+            ? this.setState({ tags: this.state.tags.filter(id => id !== tag._id) })
+            : this.setState({ tags: [...this.state.tags, tag._id] });
 
     render() {
         return (
@@ -21,12 +38,12 @@ class GameForm extends Component {
                         Game Title
                     </label>
                     <input 
-                    type="text" 
-                    id="name"
-                    name="name" 
-                    placeholder="Full game title"
-                    value={this.state.name}
-                    onChange={this.handleChange}
+                        type="text" 
+                        id="name"
+                        name="name" 
+                        placeholder="Full game title"
+                        value={this.state.name}
+                        onChange={this.handleStringChange}
                     />
                 </div>
                 <div className="field">
@@ -34,13 +51,79 @@ class GameForm extends Component {
                         Game Description
                     </label>
                     <textarea 
-                    type="text" 
-                    id="description"
-                    name="description" 
-                    placeholder="Game description"
-                    value={this.state.description}
-                    onChange={this.handleChange}
+                        type="text" 
+                        id="description"
+                        name="description" 
+                        placeholder="Game description"
+                        value={this.state.description}
+                        onChange={this.handleStringChange}
                     />
+                </div>
+                <div className="three fields">
+                    <div className="field">
+                        <label htmlFor="price">
+                            Price (in cents)
+                        </label>
+                        <input 
+                            type="number" 
+                            id="price"
+                            name="price"
+                            value={this.state.price}
+                            onChange={this.handleNumberChange}
+                        />
+                    </div>
+                    <div className="field">
+                        <label htmlFor="duration">
+                            Duration (in min)
+                        </label>
+                        <input 
+                            type="number" 
+                            id="duration"
+                            name="duration"
+                            value={this.state.duration}
+                            onChange={this.handleNumberChange}
+                        />
+                    </div>
+                    <div className="field">
+                        <label htmlFor="players">
+                            Players
+                        </label>
+                        <input 
+                            type="text" 
+                            id="players"
+                            name="players"
+                            value={this.state.players}
+                            onChange={this.handleStringChange}
+                        />
+                    </div>
+                </div>
+                <div className="inline field">
+                    <input
+                        id="featured"
+                        name="featured"
+                        type="checkbox"
+                        checked={this.state.featured}
+                        onChange={this.handleCheckboxChange}
+                    />
+                    <label htmlFor="featured">
+                        Featured?
+                    </label>
+                </div>
+                <div className="field">
+                    <label>Tags</label>
+                    {
+                        tags.map(tag => (
+                            <div key={tag._id} className="inline field">
+                                <input
+                                    id={`tag-${tag._id}`}
+                                    type="checkbox"
+                                    checked={this.state.tags.includes(tag._id)}
+                                    onChange={() => this.toggleTag(tag)}
+                                />
+                                <label htmlFor={`tag-${tag._id}`}>{tag.name}</label>
+                            </div>
+                        ))
+                    }
                 </div>
                 <button className="ui button" type="submit">
                     Create
