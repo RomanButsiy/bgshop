774300c27dd18f18f316962d0d9fbaafb9c2f6be
RomanButsiy <butsiyroman@gmail.com>
2019-06-17 20:42:54 +0300

    Define Routes with <Route /> Component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
 import * as serviceWorker from './serviceWorker';
 import App from './components/App'
 import 'semantic-ui-css/semantic.min.css';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>, 
+        document.getElementById('root'));
 
 serviceWorker.unregister();
