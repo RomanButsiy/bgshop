33b01f053146f43851fe88ece572e1e372f2588d
Estren <38942635+Estren@users.noreply.github.com>
2019-06-18 22:16:17 +0300

    Free games are allowed now

--- bgshop-api/src/routes/authgames.js
+++ bgshop-api/src/routes/authgames.js
@@ -12,7 +12,7 @@ const validate = data => {
   if (!data.players) errors.players = "This field can't be blank";
   if (!data.publisher) errors.publisher = 'You must choose publisher';
   if (!data.thumbnail) errors.thumbnail = "This field can't be blank";
-  if (data.price <= 0) errors.price = "Too cheap, don't you think?";
+  if (data.price < 0) errors.price = "Too cheap, don't you think?";
   if (data.duration <= 0) errors.duration = "Too short, isn't it?";
 
   return errors;
